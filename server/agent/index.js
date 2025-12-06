dd152e558010448d71e8021622ffb67975a3edcd
Jeman-Kumar-97-06 <jeman060397@gmail.com>
2025-12-06 19:55:48 +0530

    made changes

--- server/agent/index.js
+++ server/agent/index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import cors from 'cors';
+import { agent } from './agent.js';
 
 const app  = express();
 const PORT = 3001;
@@ -8,7 +9,19 @@ app.use(express.json());
 app.use(cors({origin:'*'}));
 
 app.get('/',(req,res)=>{
+    res.send('Hello Dawg!')
+})
 
+app.post('/generate',async (req,res)=>{
+    const result = await agent.invoke({
+        messages:[{
+            role:"user",
+            content:'Whats the weather in Tokyo'
+        }]
+    },{
+        configurable:{thread_id:42}
+    })
+    res.json(result.messages.at(-1)?.content)
 })
 
 app.listen(PORT,()=>{
